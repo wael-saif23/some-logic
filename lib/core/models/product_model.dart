7a57fa377ccbf9a74081fd9480c4a5bdd9edb733
Wael Saif <ws.mywork@gmail.com>
2025-07-08 17:19:59 +0300

    Add search functionality and product filtering to SearchPage
    - Implemented a search bar to filter products by title.
    - Added category filter buttons for product categories.
    - Included a popup menu for sorting products by name and price.
    - Created a reset filters option to restore the original product list.
    - Enhanced UI with improved layout and styling for better user experience.

--- lib/core/models/product_model.dart
+++ lib/core/models/product_model.dart
@@ -2,6 +2,14 @@ class ProductModel {
   final String id;
   final String title;
   final String image;
+   num? price;
+  final String? category;
 
-  ProductModel({required this.id, required this.title, required this.image});
+  ProductModel({
+    required this.id,
+    required this.title,
+    required this.image,
+    this.price,
+    this.category,
+  });
 }
